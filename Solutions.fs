7942a41027a230dfa1eaa0a6d64f920b0316d3c1
KarbonKitty <308lapua.magnum@gmail.com>
2018-11-23 14:37:02 +0100

    Solution to problem 8

--- Solutions.fs
+++ Solutions.fs
@@ -28,3 +28,8 @@ let problem6 limit =
 
 let problem7 n =
     naturalNumbersFrom 2L |> Seq.filter isPrime |> Seq.skip (n-1) |> Seq.take 1 |> Seq.find (fun _ -> true)
+
+let problem8 a b =
+    let x = a.ToString().ToCharArray() |> Array.map (fun y -> bigint (int (y.ToString())))
+    let mult a b = [0 .. Array.length a - b] |> List.map (fun c -> a.[c..c+b-1] |> Array.reduce (fun (x:bigint) y -> x * y))
+    mult x b |> List.max
